bc7c9063f3f354c5a2d11341dad474f0913775b5
Noah <bestofmau@gmail.com>
2022-03-24 17:51:41 +0100

    I forgot to commit these

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let length = arr.length;
+    let num = arguments.length;
+    for (let i = 1; i < num; i++) {
+        for (let j = 0; j < length; j++) {
+            if (arr[j] === arguments[i]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b) || (a < 0) || (b < 0)) {
+        return 'ERROR';
+    }
 
+    if (a >= b) {
+        while (b <= a) {
+            sum += b;
+            b++;
+        }
+    } else if (a < b) {
+        while (a <= b) {
+            sum += a;
+            a++;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
